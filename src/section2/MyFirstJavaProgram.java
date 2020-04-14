9a090e3740d80d6845f87e183cf501eb9d28f77e
root <root@4b30f0f829e2>
2020-04-14 21:58:58 +0000

    Second day of Java class. I learned how to make squares and spirals with different colors.

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,6 +8,15 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+	Robot	ironGiant = new Robot(); 
+	ironGiant.setSpeed(50);
+	ironGiant.penDown();
+	/*Hello, this is a block comment*/
+	for (int i = 0; i < 4; i++) {
+	ironGiant.move(200);	
+	ironGiant.turn(90);
+	}
+	
 	
 		
 		
